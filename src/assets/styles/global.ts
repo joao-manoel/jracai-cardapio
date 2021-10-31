0c7714c491bbe0a5e7657ff6abaccced86e79a58
Manoel <jmsneto120@gmail.com>
2021-10-30 21:04:31 -0400

    feat(styles): added size fonts

--- src/assets/styles/global.ts
+++ src/assets/styles/global.ts
@@ -4,11 +4,24 @@ export default createGlobalStyle`
   *{
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
   }
 
   html, body{
     height: 100vh;
-    width: 100vh;
+    width: 100vw;
+  }
+
+  @media (max-width: 1080px){
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720px){
+    html {
+      font-size: 87.5%;
+    }
   }
 
   body{
@@ -17,6 +30,11 @@ export default createGlobalStyle`
 
    font-family: ${props => props.theme.text.fontFamily};
    font-weight: ${props => props.theme.text.weight.medium};
+  }
+
+  a{
+    color: inherit;
+    text-decoration: none;
   }
 
 
