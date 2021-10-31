088ff4264df79c88046b7addbbfdeb8c1534cb75
Manoel <jmsneto120@gmail.com>
2021-10-30 20:05:10 -0400

    feat(core): create _document and added import fontsFamily

---
+++ src/pages/_document.tsx
@@ -0,0 +1,54 @@
+import React, { ReactElement } from "react";
+import Document, { DocumentContext, Html, Main, Head, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+ 
+
+  render(): ReactElement {
+    return (
+      <Html lang="pt_Br">
+        <Head>
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="description" content="JR AÇAI - Cardapio" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;300;500;800&display=swap" rel="stylesheet" />
+          
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
