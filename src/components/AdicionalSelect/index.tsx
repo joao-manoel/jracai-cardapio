558a4c405c536056a5d3ea51f4e7607bbb4bbb1a
Manoel <jmsneto120@gmail.com>
2021-11-01 22:35:50 -0400

    feat(product) create component select adicionais

---
+++ src/components/AdicionalSelect/index.tsx
@@ -0,0 +1,111 @@
+import React, {useState} from 'react';
+import { toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { Container } from './styles';
+
+
+type CartProps = {
+  name: string;
+  amount: number;
+}
+
+type AdicionaisProps = {
+  name: string;
+  icon?: string;
+}
+
+interface AdicionalSelect {
+  allAdicionais: Array<AdicionaisProps>;
+  qtn_adicionais: number;
+  product_name: string;
+}
+
+function AdicionalSelect({allAdicionais, qtn_adicionais, product_name} : AdicionalSelect) {
+
+  const [cart, setCart] = useState<CartProps[]>([]);
+
+  const AmountAdicional = (name: string) => {
+    const findAdicional = cart.find(a => a.name === name);
+    const amount = findAdicional?.amount;
+
+    if(amount){
+      return amount;
+    }
+
+    return 0
+  }
+
+  const addAdicional = (name: string) => {    
+    const findAdicional = cart.find(a => a.name === name);
+    const amountAdicionais = cart.length;
+
+    if(amountAdicionais >= qtn_adicionais){
+      toast.error(`Voce ja selecionou ${qtn_adicionais} adicionais`);
+      return;
+    }
+
+    if(findAdicional){
+      const amount = findAdicional.amount + 1
+
+      if(amount >= 1){
+        toast.error('Voce ja selecionou esse adicional');
+        return;
+      }
+
+      updateAdicionalAmount(name, amount)
+      return;
+    }
+
+    const amount = 1;
+
+    const newCart = [...cart, {amount, name}]
+    setCart(newCart)
+    toast.success(`Adicional ${amount}x ${name} adicionado com sucesso`)
+  }
+
+  const updateAdicionalAmount = (name: string, amount: number) => {
+    if(amount < 1){
+      return;
+    }
+
+    const updateAmountAdicional = [...cart].filter( a => {
+      if(a.name === name){
+        a.amount = amount;
+      }
+
+      return a;
+    })
+
+    setCart(updateAmountAdicional)
+    toast.success(`Adicional ${amount}x ${name} adicionado com sucesso`)
+  }
+
+  return (
+    <Container>
+      <header>
+        <h4>
+          1 {product_name}
+        </h4>
+        <p>Selecione até {qtn_adicionais} adicionais.</p>
+      </header>
+
+      <ul>
+        {allAdicionais.map(adicional => (
+          <li key={adicional.name}>
+            <div>
+              <p>{adicional.name}</p>
+            </div>
+            <div>
+              <button type="button" id="dec">-</button>
+              <input type="text" value={AmountAdicional(adicional.name)}  disabled/>
+              <button type="button" id="acre" onClick={() => addAdicional(adicional.name)} >+</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
+}
+
+export default AdicionalSelect;
---
+++ src/components/AdicionalSelect/styles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+   background: ${props => props.theme.colors.secundary};
+   border-bottom-left-radius: 20px; 
+   border-bottom-right-radius: 20px; 
+
+  header{ 
+    background: ${props => props.theme.colors.background};
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 7px;
+
+    p{
+      font-size: .9rem;
+      color: #ccc;
+      font-weight: ${props => props.theme.text.weight.light}
+    }
+  }
+
+  ul{
+    li{
+      list-style: none;
+      padding: 7px;
+
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      div:first-child{
+        font-size: 1.2em;
+        width: 70%;
+
+        p{ 
+          font-weight: ${props => props.theme.text.weight.medium};
+          color: #f9f9f9;
+        }
+      }
+
+      div:last-child{
+        width: 30%;
+
+        button, input{
+          width: 33.3%;
+          height: 40px;
+          padding: 4px;
+          border: none;
+          text-align: center;
+          color: #fff;
+        }
+
+        button:first-child{
+          background: ${props => props.theme.colors.error};
+          border-bottom-left-radius: 4px;
+          border-top-left-radius: 4px;
+        }
+        button:last-child{
+          background: ${props => props.theme.colors.success};
+          border-bottom-right-radius: 4px;
+          border-top-right-radius: 4px;
+        }
+        input{
+          background: ${props => props.theme.colors.background};
+        }
+      }
+    }
+  }
+`;
